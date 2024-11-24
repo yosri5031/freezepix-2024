0b8548a86406b3746454e0c9eccf5ffeaa099eaa
yosri ben salem <yossri.bensalem@isima.u-monastir.tn>
2024-11-24 16:50:09 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2327,11 +2327,9 @@ const countryCodeMap = {
             ) : selectedCountry === 'CAN' || selectedCountry === 'CA' ? (
               <div className="space-y-4">
                 <div className="p-4 bg-gray-50 rounded-lg">
-                  <h3 className="text-lg font-semibold mb-4">{t('canada.options')}</h3>
                   
                   {/* Sélection du mode de paiement */}
                   <div className="mb-4">
-                    <h4 className="font-medium">{t('canada.select')}</h4>
                     
                     <label className="block">
                       <input
