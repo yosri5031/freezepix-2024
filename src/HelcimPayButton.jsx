294d2d035e6d506eaf76c434f2776380fa79e574
yosri ben salem <yossri.bensalem@isima.u-monastir.tn>
2024-12-13 13:34:16 +0100

    upd orderdata

--- src/HelcimPayButton.jsx
+++ src/HelcimPayButton.jsx
@@ -6,7 +6,7 @@ const HelcimPayButton = ({
     onPaymentSuccess,
     isProcessing,
     disabled,
-    formData, 
+    orderData, 
     selectedCountry, 
     selectedPhotos,
     orderNote,
@@ -136,7 +136,7 @@ const HelcimPayButton = ({
     const handlePayment = async () => {
       try {
         // Initialize checkout
-        await initializeHelcimPayCheckout(formData);
+        await initializeHelcimPayCheckout(orderData);
   
         // Open Helcim Pay modal
         if (window.appendHelcimPayIframe && checkoutToken) {
