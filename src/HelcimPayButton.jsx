e995825820f4a3969dcab3e818996198917b87e6
yosri ben salem <yossri.bensalem@isima.u-monastir.tn>
2024-12-22 18:41:12 +0100

    Revert "Update HelcimPayButton.jsx"
    This reverts commit 6fcfa91859cf9a1ac5c368c17f075bfd3959852e.

--- src/HelcimPayButton.jsx
+++ src/HelcimPayButton.jsx
@@ -21,7 +21,6 @@ const HelcimPayButton = ({
   const [localProcessing, setLocalProcessing] = useState(false);
   const secretTokenRef = useRef(null);
   const scriptRef = useRef(null);
-  const observerRef = useRef(null);
 
   // Reset processing state when component unmounts or on abort
   useEffect(() => {
@@ -31,38 +30,21 @@ const HelcimPayButton = ({
     };
   }, [setIsProcessingOrder]);
 
-  // Monitor iframe removal
+  // Add click event listener for the back arrow
   useEffect(() => {
-    const resetStates = () => {
-      setLocalProcessing(false);
-      setIsProcessingOrder(false);
-      setCheckoutToken(null);
-      secretTokenRef.current = null;
-    };
-
-    observerRef.current = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        mutation.removedNodes.forEach((node) => {
-          // Check if the removed node is the Helcim iframe
-          if (node.nodeName === 'IFRAME' && node.id === 'helcim-pay-frame') {
-            console.log('Helcim iframe removed');
-            resetStates();
-          }
-        });
-      });
-    });
-
-    // Start observing the document body for iframe removal
-    observerRef.current.observe(document.body, {
-      childList: true,
-      subtree: true
-    });
-
-    return () => {
-      if (observerRef.current) {
-        observerRef.current.disconnect();
+    const handleBackArrowClick = (event) => {
+      // Check if the clicked element is the back arrow or its parent
+      const isBackArrow = event.target.closest('.fa-arrow-left');
+      if (isBackArrow) {
+        setLocalProcessing(false);
+        setIsProcessingOrder(false);
+        setCheckoutToken(null);
+        secretTokenRef.current = null;
       }
     };
+
+    document.addEventListener('click', handleBackArrowClick);
+    return () => document.removeEventListener('click', handleBackArrowClick);
   }, [setIsProcessingOrder]);
 
   // Load Helcim Pay.js script
