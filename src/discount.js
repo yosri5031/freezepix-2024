05a8803ecbe5d57b8d6905291a1f67515a91cded
yosri ben salem <yossri.bensalem@isima.u-monastir.tn>
2025-01-03 12:14:37 +0100

    Update discount.js

--- src/discount.js
+++ src/discount.js
@@ -1,46 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const shopifyStoreUrl = 'https://freezepix.myshopify.com';
-const accessToken = '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';
-
 const fetchDiscountCodes = async () => {
   try {
-    const response = await axios.get(`${shopifyStoreUrl}/admin/api/2024-01/price_rules.json`, {
-      headers: {
-        'X-Shopify-Access-Token': accessToken,
-        'Content-Type': 'application/json'
-      }
-    });
-
-    const priceRules = response.data.price_rules;
-    const discountCodes = [];
-
-    for (const rule of priceRules) {
-      const codeResponse = await axios.get(
-        `${shopifyStoreUrl}/admin/api/2024-01/price_rules/${rule.id}/discount_codes.json`,
-        {
-          headers: {
-            'X-Shopify-Access-Token': accessToken,
-            'Content-Type': 'application/json'
-          }
-        }
-      );
-
-      codeResponse.data.discount_codes.forEach(code => {
-        discountCodes.push({
-          code: code.code,
-          value: rule.value,
-          valueType: rule.value_type,
-          title: rule.title
-        });
-      });
-    }
-
-    return discountCodes;
+    const response = await axios.get('https://freezepix-database-server-c95d4dd2046d.herokuapp.com/api/discount-codes');
+    return response.data;
   } catch (error) {
     console.error('Error fetching discount codes:', error);
     return [];
   }
 };
-export default fetchDiscountCodes;
+
+const DiscountCodesProvider = ({ children }) => {
+  const [discountCodes, setDiscountCodes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const codes = await fetchDiscountCodes();
+      setDiscountCodes(codes);
+    };
+
+    fetchData();
+  }, []);
+
+  return <>{children(discountCodes)}</>;
+};
+
+export default DiscountCodesProvider;
