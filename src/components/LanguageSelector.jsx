8738e2d76c88bc7925204305d135334621a9a935
yosri ben salem <yossri.bensalem@isima.u-monastir.tn>
2025-01-09 10:01:07 +0100

    lang url

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLanguage } from '../contexts/LanguageContext';
+import { useHistory } from 'react-router-dom';
 
 const LanguageSelector = () => {
   const { changeLanguage, language } = useLanguage();
+  const history = useHistory();
+
+  const handleLanguageChange = (newLanguage) => {
+    changeLanguage(newLanguage);
+    
+    // Redirect to the appropriate route based on the selected language
+    if (newLanguage === 'fr') {
+      history.push('/fr'); // Redirect to French home
+    } else {
+      history.push('/'); // Redirect to English home
+    }
+  };
+
+  // Optional: If you want to update the URL when the component mounts
+  useEffect(() => {
+    if (language === 'fr') {
+      history.push('/fr');
+    } else {
+      history.push('/');
+    }
+  }, [language, history]);
 
   return (
     <div className="language-selector">
       <select 
         value={language} 
-        onChange={(e) => changeLanguage(e.target.value)}
+        onChange={(e) => handleLanguageChange(e.target.value)}
         className="p-2 border rounded"
       >
         <option value="en">English</option>
@@ -18,4 +40,4 @@ const LanguageSelector = () => {
   );
 };
 
-export default LanguageSelector; 
+export default LanguageSelector;
