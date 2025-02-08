fae2a5a809978c12e77b757c10e7b67e37967729
yosri ben salem <yossri.bensalem@isima.u-monastir.tn>
2025-02-08 17:22:24 +0100

    try

--- src/App.jsx
+++ src/App.jsx
@@ -3407,7 +3407,7 @@ if (showIntro) {
                       <span className="text-black">freeze</span>
                       <span className="text-yellow-400">PIX</span>
                     </div>
-                    <LanguageSelector />
+                    <LanguageSelector key={language} />
                   </div>
 
                   <div className="text-sm italic text-gray-600 mb-8">
