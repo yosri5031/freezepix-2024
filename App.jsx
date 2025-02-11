3bd1d1bb44042307990641b4809e8fa54577eee1
yosri ben salem <yossri.bensalem@isima.u-monastir.tn>
2025-02-11 15:45:22 +0100

    Revert "Update App.jsx"
    This reverts commit 15cf52335ddf0202d0dd28df2fb554a690f1b4b1.

--- App.jsx
+++ App.jsx
@@ -2636,8 +2636,12 @@ const validateStep = () => {
           const shippingAddress = formData.shippingAddress;
           return Boolean(
             formData.email &&
-            formData.phone 
-            
+            formData.phone &&
+            shippingAddress.firstName &&
+            shippingAddress.lastName &&
+            shippingAddress.address &&
+            shippingAddress.city &&
+            shippingAddress.postalCode
           );
         }
       }
