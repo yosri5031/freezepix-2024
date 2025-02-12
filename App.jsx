8fd5ed9a018806943e0f0ffd4bf0d881372bdd38
yosri ben salem <yossri.bensalem@isima.u-monastir.tn>
2025-02-12 22:12:45 +0100

    Revert "Update App.jsx"
    This reverts commit 3d1e547041139036ea928f30dafabdb5e36256c5.

--- App.jsx
+++ App.jsx
@@ -1883,11 +1883,7 @@ const PaymentForm = ({ onPaymentSuccess }) => {
     if (!isOrderOverThreshold) {
         if (selectedCountry === 'TUN') {
             shippingFee = 8; // 8 TND for Tunisia
-                } 
-        else if (formData.deliveryMethod === 'pickup') {
-          shippingFee = 0;
-        }
-        else if (selectedCountry === 'USA') {
+        } else if (selectedCountry === 'USA') {
             shippingFee = 9; // 9$ for USA
         } else if (selectedCountry === 'CAN') {
             shippingFee = 9; // 9$ for Canada
